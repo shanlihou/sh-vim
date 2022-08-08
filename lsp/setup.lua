5d3f19cf57af8594dc4abb2118e23799dcad307e
shanlihou <shanlihou@gmail.com>
2022-08-08 14:38:10 +0800

    add cpp lsp

--- lsp/setup.lua
+++ lsp/setup.lua
@@ -6,6 +6,7 @@ local lsp_installer = require "nvim-lsp-installer"
 local servers = {
   sumneko_lua = require "lsp.lua", -- /lua/lsp/lua.lua
   pyright = require "lsp.pyright",
+  ccls = require "lsp.cpp",
 }
 
 -- 自动安装 LanguageServers
