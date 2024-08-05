73627de2df3bf06cdbc77a9b3f12493d9ef674ec
shanlihou <shanlihou@gmail.com>
2024-08-05 17:28:07 +0800

    up

--- keybindings.lua
+++ keybindings.lua
@@ -38,7 +38,7 @@ vim.keymap.set({ "n", "v" }, "mm", "<cmd>BookmarksMark<cr>", { desc = "Mark curr
 vim.keymap.set({ "n", "v" }, "mo", "<cmd>BookmarksGoto<cr>", { desc = "Go to bookmark at current active BookmarkList" })
 vim.keymap.set({ "n", "v" }, "ma", "<cmd>BookmarksCommands<cr>", { desc = "Find and trigger a bookmark command." })
 vim.keymap.set({ "n", "v" }, "mg", "<cmd>BookmarksGotoRecent<cr>", { desc = "Go to latest visited/created Bookmark" })
-vim.keymap.set({ "n", "v" }, "md", "<cmd>BookmarksDisplay<cr>", { desc = "display all the bookmarks" })
+vim.keymap.set({ "n", "v" }, "md", "<cmd>BookmarksTree<cr>", { desc = "display all the bookmarks" })
 
 -- session
 map('n', '<leader>so', ':OpenSession ', opt)
