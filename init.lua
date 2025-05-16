3e2b674975d84802c5ae13cc2ecf80c6d7f356ff
shanlihou <shanlihou@gmail.com>
2025-05-16 15:45:51 +0800

    up

--- init.lua
+++ init.lua
@@ -15,7 +15,7 @@ require('plugin-config/multi')
 require('plugin-config/hydra')
 require('plugin-config/telescope')
 require('plugin-config/dropbar')
-require('plugin-config/fittencode')
+require('plugin-config/fitten')
 if (vim.fn.has('win32') == 0)
 then
     require('plugin-config/nvim-treesitter')
