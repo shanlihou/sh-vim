acb67553e199a896646a8b70410ff5a98b69573d
shanlihou <shanlihou@gmail.com>
2026-01-22 15:49:07 +0800

    up

--- basic/auto_cmd.lua
+++ basic/auto_cmd.lua
@@ -146,3 +146,10 @@ local function copy()
 end
 
 vim.api.nvim_create_autocmd('TextYankPost', {callback = copy})
+
+
+if vim.g.neovide then
+    vim.keymap.set({ "n", "v" }, "<C-+>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>")
+    vim.keymap.set({ "n", "v" }, "<C-->", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>")
+    vim.keymap.set({ "n", "v" }, "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>")
+end
