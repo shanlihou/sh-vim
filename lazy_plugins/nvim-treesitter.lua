803af7794fd8371d00fd9c7c13c776bbed9ef274
shanlihou <shanlihou@gmail.com>
2026-05-01 09:51:04 +0800

    up treesitter

--- lazy_plugins/nvim-treesitter.lua
+++ lazy_plugins/nvim-treesitter.lua
@@ -5,14 +5,13 @@ if (vim.fn.has('win32') == 0) then
     -- 默认不要折叠
     -- https://stackoverflow.com/questions/8316139/how-to-set-the-default-to-unfolded-when-you-open-a-file
     vim.wo.foldlevel = 99
-
-
     return {
         'nvim-treesitter/nvim-treesitter',
+        build = ":TSUpdate", -- 建议加上这一行，插件更新时会自动更新 parser
         config = function()
-            require'nvim-treesitter.configs'.setup {
+            -- 重点：这里去掉了 .configs
+            require('nvim-treesitter').setup({
                 -- 安装 language parser
-                -- :TSInstallInfo 命令查看支持的语言
                 ensure_installed = {"html", "css", "vim", "lua", "javascript", "typescript", "tsx", "python", "cpp", "c", "vimdoc"},
                 -- 启用代码高亮功能
                 highlight = {
@@ -23,19 +22,20 @@ if (vim.fn.has('win32') == 0) then
                 incremental_selection = {
                     enable = true,
                     keymaps = {
-                    init_selection = '<CR>',
-                    node_incremental = '<CR>',
-                    node_decremental = '<BS>',
-                    scope_incremental = '<TAB>',
+                        init_selection = '<CR>',
+                        node_incremental = '<CR>',
+                        node_decremental = '<BS>',
+                        scope_incremental = '<TAB>',
                     }
                 },
-                -- 启用基于Treesitter的代码格式化(=) . NOTE: This is an experimental feature.
+                -- 启用缩进
                 indent = {
                     enable = true
                 }
-            }
+            })
         end
     }
+
 else
     return {}
 end
